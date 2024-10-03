5134539e333497e33e5ee1a474141896ea3075d1
Alex Lindström <al223gv@student.lnu.se>
2024-10-03 02:36:02 +0200

    Move methods that can be shared to a util class

---
+++ src/lib/TextAnalyzerUtils.ts
@@ -0,0 +1,42 @@
+export class TextAnalyzerUtils {
+  assertIsString (value: unknown): void {
+    if (typeof value !== 'string') {
+      throw new TypeError('Value is not a string.')
+    }
+  }
+
+  assertIsPositiveNumber (value: unknown): void {
+    if (typeof value !== 'number' || !Number.isFinite(value) || value < 0) {
+      throw new TypeError('Value is not a positive number.')
+    }
+  }
+
+  removeNonLatinExceptAllowed (text: string, allowed?: string): string {
+    const allowedCharacters = allowed ?? ''
+
+    const disallowedCharactersRegex = new RegExp(`[^a-zA-Z${allowedCharacters}]`, 'g')
+
+    const cleanedText = text.replace(disallowedCharactersRegex, '')
+
+    return cleanedText
+  }
+
+  stripMultipleSpaces (text: string): string {
+    const multipleSpaces = new RegExp('\\s+', 'g')
+
+    const singleSpaced = text.replace(multipleSpaces, ' ')
+      .trim()
+    
+    return singleSpaced
+  }
+
+  extractWordsFromText (text: string): string[] {
+    const textWithoutNonLatinCharacters = this.removeNonLatinExceptAllowed(text)
+
+    const textWithSingleSpaces = this.stripMultipleSpaces(textWithoutNonLatinCharacters)
+
+    const words = textWithSingleSpaces.split(' ')
+
+    return words
+  }
+}
