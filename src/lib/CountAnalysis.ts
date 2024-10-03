9e6e3754815a25fa32c8e5d9c802843a531ff82d
Alex Lindström <al223gv@student.lnu.se>
2024-10-03 02:40:30 +0200

    Add methods and break into smaller methods in CountAnalysis

--- src/lib/CountAnalysis.ts
+++ src/lib/CountAnalysis.ts
@@ -1,21 +1,16 @@
+import { TextAnalyzerUtils } from "./TextAnalyzerUtils.js"
+
 export class CountAnalysis {
-  #nonLatinCharacters: RegExp
+  #utils: TextAnalyzerUtils
 
   constructor () {
-    this.#nonLatinCharacters = new RegExp('[^a-zA-Z ]', 'g')
-  }
-
-  #assertIsString (text: unknown) {
-    if (typeof text !== 'string') {
-      throw new TypeError('Text is not of type string.')
-    }
+    this.#utils = new TextAnalyzerUtils()
   }
 
   numberOfWords (text: string): number {
-    this.#assertIsString(text)
+    this.#utils.assertIsString(text)
 
-    const cleanedText = text.replaceAll(this.#nonLatinCharacters, ' ').trim()
-    const words = cleanedText.split(/\s+/)
+    const words = this.#utils.extractWordsFromText(text)
 
     let numberOfWords = 0
     for (const word of words) {
@@ -28,10 +23,9 @@ export class CountAnalysis {
   }
 
   numberOfPeriods (text: string): number {
-    this.#assertIsString(text)
+    this.#utils.assertIsString(text)
 
     let numberOfPeriods = 0
-
     for (const character of text) {
       if (character === '.') {
         numberOfPeriods++
@@ -39,5 +33,62 @@ export class CountAnalysis {
     }
 
     return numberOfPeriods
+  }
+
+  numberOfCharactersIncludingSpaces (text: string): number {
+    this.#utils.assertIsString(text)
+
+    return text.length
+  }
+
+  numberOfCharactersExcludingSpaces (text: string): number {
+    this.#utils.assertIsString(text)
+
+    const textWithoutSpace = text.replaceAll(' ', '')
+
+    return textWithoutSpace.length
+  }
+
+  numberOfVowels (text: string): number {
+    this.#utils.assertIsString(text)
+
+    const vowelRegex = new RegExp('[eoaiu]', 'gi')
+
+    const vowels = text.match(vowelRegex) ?? []
+
+    return vowels.length
+  }
+
+  _removeNonLatinExceptAllowed (text: string, allowedCharacters: string): string {
+    const disallowedCharactersRegex = new RegExp(`[^a-zA-Z${allowedCharacters}]`, 'g')
+
+    const cleanedText = text.replace(disallowedCharactersRegex, '')
+
+    return cleanedText
+  }
+
+  numberOfSentences (text: string, customAbbreviations: string[]): number {
+    this.#utils.assertIsString(text)
+
+    const sentenceEndingPattern = new RegExp('[a-zA-Z ]+[.!?]', 'g')
+
+    const cleanedText = this._removeNonLatinExceptAllowed(text, ' .!?')
+
+    const abbreviationStrippedText = this._stripCustomAbbreviation(
+      cleanedText, customAbbreviations)
+
+    const numberOfSentences = abbreviationStrippedText
+      .match(sentenceEndingPattern) ?? []
+
+    return numberOfSentences.length
+  }
+
+  _stripCustomAbbreviation (text: string, customAbbreviations: string[]): string {
+    let result = text
+    for (const abbreviation of customAbbreviations) {
+      result = result.replaceAll(abbreviation, ' ')
+    }
+
+    return result
   }
 }
