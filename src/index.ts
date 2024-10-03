3deb5a1d17a1bcebb6d7310875fca8fcedc775cb
Alex Lindström <al223gv@student.lnu.se>
2024-10-03 02:38:34 +0200

    Clean up index to only import/export TextAnalyzer

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-import { TextAnalyzer } from "./TextAnalyzer.js";
+import { TextAnalyzer } from "./lib/TextAnalyzer.js";
 
-const textAnalyzer = new TextAnalyzer()
-
-console.log(textAnalyzer.count.numberOfWords('hello, hello, hello'))
-console.log(textAnalyzer.count.numberOfWords('low!high croocodile,,,,KepS.,.,.!!Gris'))
-console.log(textAnalyzer.count.numberOfPeriods('low!high croocodile,,,,KepS.,.,.!!Gris'))
-console.log(textAnalyzer.count.numberOfPeriods('...gdf..df.'))
+export { TextAnalyzer }
